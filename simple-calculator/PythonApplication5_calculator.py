2c918e414efd9569c5791210ca4935d36919b8f6
Alaaalfadil-software <alaa90_rad@hotmail.com>
2025-07-10 00:54:20 +0400

    Update PythonApplication5_calculator.py

--- simple-calculator/PythonApplication5_calculator.py
+++ simple-calculator/PythonApplication5_calculator.py
@@ -1,10 +1,8 @@
 #Simple Calculator -Sum of Two Numbers
-from ast import Mult
 
-
-print("welcome to alaa calculator")
+print("Welcome to Alaa's Calculator 😊")
 Num1 =input("Enter the First Num:   ")
-Num2 =input("Enter the secand Num:  ")
+Num2 =input("Enter the second Num:  ")
 Num1=float(Num1)
 Num2=float(Num2)
 operator=str (input("Enter your operato * / + - :  "))
